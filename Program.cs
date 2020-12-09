dc35a5bc734127a090926ec60c84c74917240690
Unfog <zaytsev.moon@gmail.com>
2020-12-09 12:30:16 +0300

    test action

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TestCI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World123!");
         }
     }
 }
