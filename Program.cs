fe3f509462ffade12a5f7acc158aebdcdc788c80
Unfog <zaytsev.moon@gmail.com>
2020-12-09 20:56:00 +0300

    234

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TestCI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell12321313234sdfo World13!");
+            Console.WriteLine("Hell1232131323dfg4sdfo World13!");
         }
     }
 }
